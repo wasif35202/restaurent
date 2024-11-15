1042df3bf3f79bd7b88fcc4d7efd5f0c912f203f
wasif35202 <145279916+wasif35202@users.noreply.github.com>
2024-11-15 19:59:14 +0500

    Update

--- app/payment/api/create-payment-intent/[id]/route.ts
+++ app/payment/api/create-payment-intent/[id]/route.ts
@@ -4,7 +4,10 @@ import prisma from '@/utils/utils';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
-export async function POST(req: NextRequest,   context: { params: { id: string } }) {
+export const POST = async (
+  req: NextRequest,
+  context: { params: { id: string } }
+) => {
   const { amount } = await req.json(); // Read the request body
   const { id } = context.params;
   try {
@@ -16,18 +19,17 @@ export async function POST(req: NextRequest,   context: { params: { id: string }
 
     await prisma.order.update({
       where: {
-        id:id,
+        id: id,
       },
       data: {
         intent_Id: paymentIntent.id,
       },
     });
 
-  return NextResponse.json({
-  clientSecret: paymentIntent.client_secret,
-  paymentIntentId: paymentIntent.id, // add this to pass ID to frontend if needed
-});
-
+    return NextResponse.json({
+      clientSecret: paymentIntent.client_secret,
+      paymentIntentId: paymentIntent.id, // add this to pass ID to frontend if needed
+    });
   } catch (error) {
     const errorMessage =
       error instanceof Error ? error.message : 'An unexpected error occurred';
@@ -35,4 +37,4 @@ export async function POST(req: NextRequest,   context: { params: { id: string }
     // Return error response
     return NextResponse.json({ error: errorMessage }, { status: 500 });
   }
-}
+};
